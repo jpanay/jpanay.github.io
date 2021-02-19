8486f2e14c6db6a08b407ead57d4032f0c6b65b7
Jason Panay <jasonpanay@gmail.com>
2021-02-18 22:30:12 -0500

    Blog basic complete

--- js/main.js
+++ js/main.js
@@ -14,16 +14,16 @@ let nameAnimation = (isClick = true) => {
   $('.panay').empty()
   $('.paren').empty()
   if (isClick) {
-    audio2.play()
-    setTimeout(() => { audio2.play() }, 100)
-    setTimeout(() => { audio3.play() }, 200)
-    setTimeout(() => { audio2.play() }, 300)
-    setTimeout(() => { audio1.play() }, 400)
-    setTimeout(() => { audio1.play() }, 500)
-    setTimeout(() => { audio3.play() }, 600)
-    setTimeout(() => { audio2.play() }, 700)
-    setTimeout(() => { audio2.play() }, 800)
-    setTimeout(() => { }, 900)
+    // audio2.play()
+    // setTimeout(() => { audio2.play() }, 100)
+    // setTimeout(() => { audio3.play() }, 200)
+    // setTimeout(() => { audio2.play() }, 300)
+    // setTimeout(() => { audio1.play() }, 400)
+    // setTimeout(() => { audio1.play() }, 500)
+    // setTimeout(() => { audio3.play() }, 600)
+    // setTimeout(() => { audio2.play() }, 700)
+    // setTimeout(() => { audio2.play() }, 800)
+    // setTimeout(() => { }, 900)
   }
   setTimeout(() => { $('.j').text('j') }, 100)
   setTimeout(() => { $('.dot').text('.') }, 200)
@@ -49,11 +49,11 @@ let generateHome = () => {
   let homeInfo = $(`<div class="home-info"></div>`)
   let homeInfoTxtName = $(`<div class="home-info-txt"> Jason Panay </div>`)
   let homeInfoTxtLocation = $(`<div class="home-info-txt"> New York,New York </div>`)
-  let homeLinkGithub = $(`<div class="home-info-link">
-    <img class="img-sml" src="media/github.png"> Github
+  let homeLinkGithub = $(`<div class="home-info-link github-link">
+    <img class="img-sml github-img" src="media/github.png"> Github
   </div>`)
-  let homeLinkLinkedin = $(`<div class="home-info-link">
-    <img class="img-sml" src="media/linkedin.png"> Linkedin
+  let homeLinkLinkedin = $(`<div class="home-info-link linkedin-link">
+    <img class="img-sml linkedin-img" src="media/linkedin.png"> Linkedin
   </div>`)
 
 
@@ -65,30 +65,49 @@ let generateHome = () => {
   homeCenter.append(homeHeadshot)
   homeCenter.append(homeInfo)
 
-  let homeLeft = $(`<div class="home-center">LEFT</div>`)
 
-  let homeRight = $(`<div class="home-center">RIGHT</div>`)
+
+  let homeLeft = $(`<div class="home-left"></div>`)
+  let leftImg = $(`<img class="home-left-link" src="media/blank.png"></img>`)
+  homeLeft.append(leftImg)
+
+  let homeRight = $(`<div class="home-right"></div>`)
+  let rightImg = $(`<img class="home-right-link" src="media/blank.png"></img>`)
+  homeRight.append(rightImg)
 
   home.append(homeLeft);
   home.append(homeCenter);
   home.append(homeRight);
   // center.append(three)
 
-  // ASK
-  // let askDiv = $(`<div class="ask-div"></div>`)
-  // let askInput = $(`<input class="ask-input" placeholder="Ask me a question">`)
-  // let askBtn = $(`<button class="ask-btn"><span class="green">get</span><span class="blue">.</span><span class="yellow">answer</span><span class="green">(</span></span></span><span class="blue">input</span><span class="green">)</span></button>`)
-  // let askAnswer = $(`<p class="ask-answer">> ...</p>`)
-  // askDiv.append(askInput)
-  // askDiv.append(askBtn)
-  // askDiv.append(askAnswer)
-  // div.append(askDiv)
-  // THREE
-  // let three = $(`<div class="three" id="three"></div>`)
-  //
 
   // RENDER
   $('.app').append(home)
+
+  $('.github-link').hover((e) => {
+    $('.github-img').css('background-color', 'rgb(118, 250, 147)')
+    $('.github-img').css('transorm', 'scale(0.95)')
+  })
+
+  $('.linkedin-link').hover((e) => {
+    $('.linkedin-img').css('background-color', 'rgb(118, 250, 147)')
+    $('.linkedin-img').css('transorm', 'scale(0.95)')
+  })
+
+  $('.home-info-link').on('mouseleave' ,(e) => {
+    $('.img-sml').css('background-color', '')
+    $('.github-img').css('transform', 'scale(1)')
+  })
+
+
+  let turn1 = 0
+  let turn2 = 0
+  setInterval(() => {
+    turn1 += 0
+    turn2 -= 0
+    $('.home-left-link').css('transform', `rotate(${turn1}turn)`)
+    $('.home-right-link').css('transform', `rotate(${turn2}turn)`)
+  }, 50)
 }
 generateHome()
 
